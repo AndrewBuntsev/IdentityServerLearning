1f8f731ea9b5e0cd600602fbcf1d154ee4050b91
NNA\abuntsev <abuntsev@nine.com.au>
2019-04-23 16:23:26 +1000

    Initial Commit

---
+++ IdSrv/Scopes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using IdentityServer3.Core.Models;
+
+static class Scopes
+{
+    public static List<Scope> Get()
+    {
+        return new List<Scope>
+        {
+            new Scope
+            {
+                Name = "api1"
+            }
+        };
+    }
+}
